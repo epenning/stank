79acb141003cb5e1a6d3b4f67a04c55fcde0cffa
Erin Pennington <epennington@utexas.edu>
2016-05-18 21:26:26 -0500

    editing third person character controls

---
+++ Assets/Scripts/ThirdPersonUserControl.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class ThirdPersonUserControl : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+        private Vector3 m_Move;
+        
+        private void Start()
+        {
+            // get the third person character ( this should never be null due to require component )
+            m_Character = GetComponent<ThirdPersonCharacter>();
+        }
+
+        // Fixed update is called in sync with physics
+        private void FixedUpdate()
+        {
+            // read inputs
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+            // we use world-relative directions in the case of no main camera
+            m_Move = v*Vector3.forward + h*Vector3.right;
+
+#if !MOBILE_INPUT
+			// walk speed multiplier
+	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+#endif
+
+            // pass all parameters to the character control script
+            m_Character.Move(m_Move);
+
+        }
+    }
+}
