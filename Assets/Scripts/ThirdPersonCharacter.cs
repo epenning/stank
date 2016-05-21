29bb26bd1030760b54e9084da62a40b08b9fe5c6
Erin Pennington <epennington@utexas.edu>
2016-05-21 23:24:57 +0300

    silly turret physics and also music

--- Assets/Scripts/ThirdPersonCharacter.cs
+++ Assets/Scripts/ThirdPersonCharacter.cs
@@ -1,57 +1,54 @@
 using UnityEngine;
 
-namespace UnityStandardAssets.Characters.ThirdPerson
+[RequireComponent(typeof(Rigidbody))]
+public class ThirdPersonCharacter : MonoBehaviour
 {
-	[RequireComponent(typeof(Rigidbody))]
-	public class ThirdPersonCharacter : MonoBehaviour
+	[SerializeField] float m_MovingTurnSpeed = 180;
+	[SerializeField] float m_StationaryTurnSpeed = 90;
+	[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
+	[SerializeField] float m_MoveSpeedMultiplier = 0.1f;
+
+	Rigidbody m_Rigidbody;
+	const float k_Half = 0.5f;
+	float m_TurnAmount;
+	float m_ForwardAmount;
+	Vector3 m_GroundNormal;
+
+
+
+	void Start()
 	{
-		[SerializeField] float m_MovingTurnSpeed = 180;
-		[SerializeField] float m_StationaryTurnSpeed = 90;
-		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
-		[SerializeField] float m_MoveSpeedMultiplier = 0.1f;
-
-		Rigidbody m_Rigidbody;
-		const float k_Half = 0.5f;
-		float m_TurnAmount;
-		float m_ForwardAmount;
-		Vector3 m_GroundNormal;
+		m_Rigidbody = GetComponent<Rigidbody>();
+		m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+	}
 
 
-		void Start()
-		{
-			m_Rigidbody = GetComponent<Rigidbody>();
+	public void Move(Vector3 move)
+	{
 
-			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-		}
+		// convert the world relative moveInput vector into a local-relative
+		// turn amount and forward amount required to head in the desired
+		// direction.
+		if (move.magnitude > 1f) move.Normalize();
+		//move = Vector3.ProjectOnPlane(move, m_GroundNormal);
+		m_TurnAmount = move.x;
+		m_ForwardAmount = move.z;
 
+		ApplyExtraTurnRotation();
 
-		public void Move(Vector3 move)
-		{
+		Vector3 v = new Vector3(0,0,m_ForwardAmount) * m_MoveSpeedMultiplier / Time.deltaTime;
 
-			// convert the world relative moveInput vector into a local-relative
-			// turn amount and forward amount required to head in the desired
-			// direction.
-			if (move.magnitude > 1f) move.Normalize();
-			//move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-			m_TurnAmount = move.x;
-			m_ForwardAmount = move.z;
+		v = transform.TransformDirection(v);
 
-			ApplyExtraTurnRotation();
+		// maintain current y velocity
+		v.y = m_Rigidbody.velocity.y;
+		m_Rigidbody.velocity = v;
+	}
 
-			Vector3 v = new Vector3(0,0,m_ForwardAmount) * m_MoveSpeedMultiplier / Time.deltaTime;
-
-			v = transform.TransformDirection(v);
-
-			// maintain current y velocity
-			v.y = m_Rigidbody.velocity.y;
-			m_Rigidbody.velocity = v;
-		}
-
-		void ApplyExtraTurnRotation()
-		{
-			// help the character turn faster (this is in addition to root rotation in the animation)
-			float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
-			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
-		}
+	void ApplyExtraTurnRotation()
+	{
+		// help the character turn faster (this is in addition to root rotation in the animation)
+		float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+		transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
 	}
 }
--- Assets/Scripts/ThirdPersonUserControl.cs
+++ Assets/Scripts/ThirdPersonUserControl.cs
@@ -2,33 +2,45 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-namespace UnityStandardAssets.Characters.ThirdPerson
+[RequireComponent(typeof (ThirdPersonCharacter))]
+public class ThirdPersonUserControl : MonoBehaviour
 {
-    [RequireComponent(typeof (ThirdPersonCharacter))]
-    public class ThirdPersonUserControl : MonoBehaviour
+    private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+    private Vector3 m_Move;
+
+	public GameObject turret;
+	public GameObject camera;
+
+	MouseLook mouseLook;
+    
+    private void Start()
     {
-        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Vector3 m_Move;
-        
-        private void Start()
-        {
-            // get the third person character ( this should never be null due to require component )
-            m_Character = GetComponent<ThirdPersonCharacter>();
-        }
+        // get the third person character ( this should never be null due to require component )
+        m_Character = GetComponent<ThirdPersonCharacter>();
 
-        // Fixed update is called in sync with physics
-        private void FixedUpdate()
-        {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
+		mouseLook = new MouseLook ();
+		mouseLook.Init (turret.transform, camera.transform);
+    }
 
-            // we use world-relative directions in the case of no main camera
-			m_Move = v*Vector3.forward + h*Vector3.right;
+	private void Update()
+	{
+		// update mouse camera movement
+		mouseLook.LookRotation (turret.transform, camera.transform);
+	}
 
-            // pass all parameters to the character control script
-            m_Character.Move(m_Move);
+    // Fixed update is called in sync with physics
+    private void FixedUpdate()
+    {
+        // read inputs
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-        }
+        // we use world-relative directions in the case of no main camera
+		m_Move = v*Vector3.forward + h*Vector3.right;
+
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move);
+
+
     }
 }
