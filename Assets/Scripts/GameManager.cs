2751008a04a36319536280e45280c35a08b89f4d
Erin Pennington <epennington@utexas.edu>
2016-05-23 19:20:18 +0300

    buttons r for reset, escape to quit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.R))
+			Restart ();
+	
+		if (Input.GetKey (KeyCode.Escape))
+			Quit ();
+	}
+
+	void Restart() 
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+	void Quit() 
+	{
+		Application.Quit ();
+	}
+}
