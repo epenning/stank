919552c1aa96892140ead663e075389082f91a97
Erin Pennington <epennington@utexas.edu>
2016-05-26 23:22:49 +0300

    blocks stick to objects on collision, playable build

---
+++ Assets/Scripts/GrappleCollisions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GrappleCollisions : MonoBehaviour {
+
+	public bool inAir = false;
+	public HingeJoint grabHinge;
+
+	//	void OnCollisionEnter (Collision col) {
+	//		if (inAir = true) {
+	//			rigidbody.velocity = 0;
+	//			inAir = false;
+	//			grabHinge = gameObject.AddComponent <HingeJoint>();
+	//			grabHinge.connectedBody = col.rigidbody;
+	//			//This stops the hook once it collides with something, and creates a HingeJoint to the object it collided with.
+	//		}
+	//	}
+
+	void OnCollisionEnter (Collision col) 
+	{
+		if (inAir = true) {
+			gameObject.GetComponent<Rigidbody> ().velocity = new Vector3(0,0,0);
+			inAir = false;
+			grabHinge = gameObject.AddComponent<HingeJoint> ();
+			grabHinge.connectedBody = col.rigidbody;
+			//This stops the hook once it collides with something, and creates a HingeJoint to the object it collided with.
+		}
+	}
+
+}
